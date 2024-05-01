c511e1b7491fafea2a5546e4a05376a315ad5ac9
Huseynov Orxan Samir <Husey_so01@itstep.edu.az>
2024-05-01 08:03:51 +0400

    Anket

--- Anket-WPF_MVVM/ViewModels/AnketViewModel.cs
+++ Anket-WPF_MVVM/ViewModels/AnketViewModel.cs
@@ -3,6 +3,7 @@ using Anket_WPF_MVVM.Models;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Xml.Linq;
 
 namespace Anket_WPF_MVVM.ViewModels;
 
@@ -18,22 +19,85 @@ public class AnketViewModel : INotifyPropertyChanged
 
     public RelayCommand ?AddPerson { get; set; }
 
-
     public AnketViewModel()
     {
         Persons = new();
-        SelectedPerson = new();
+        SelectedPerson = new()
+        {
+            Name = NewPersonName,
+            Surname = NewPersonName,
+            PhoneNumber = NewPersonPhoneNumber,
+            Email = NewPersonEmail,
+            Brithday = NewPersonBirthday
+
+        };
         AddPerson = new RelayCommand(parameter =>
         {
             Persons.Add(SelectedPerson);
         });
-    
+
     }
 
-    public event PropertyChangedEventHandler? PropertyChanged;
+    private string? _newPersonName;
+    public string? NewPersonName
+    {
+        get { return _newPersonName; }
+        set
+        {
+            _newPersonName = value;
+            OnPropertyChanged();
+        }
+    }
 
-    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    private string? _newPersonSurname;
+    public string? NewPersonSurname
+    {
+        get { return _newPersonSurname; }
+        set
+        {
+            _newPersonSurname = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string? _newPersonPhoneNumber;
+    public string? NewPersonPhoneNumber
+    {
+        get { return _newPersonPhoneNumber; }
+        set
+        {
+            _newPersonPhoneNumber = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string? _newPersonEmail;
+    public string? NewPersonEmail
+    {
+        get { return _newPersonEmail; }
+        set
+        {
+            _newPersonEmail= value;
+            OnPropertyChanged();
+        }
+    }
+
+    private DateTime _newPersonBirthday = DateTime.Today;
+    public DateTime NewPersonBirthday
+    {
+        get { return _newPersonBirthday; }
+        set
+        {
+            _newPersonBirthday = value;
+            OnPropertyChanged();
+        }
+    }
+
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void OnPropertyChanged([CallerMemberName] string name = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
+    
 }
